ae373d4137c55bce0b2d1a609d46d77740af47fa
DukeGumball <dukegumball@gmail.com>
2025-03-27 09:19:16 -0400

    f

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async() => {
     const projectsButton = document.querySelector("#Projects");
+    const dropdown = document.querySelector("#DropDown");
+
+    const hoveredProject = false;
 
     projectsButton.addEventListener("mouseover", () => {
-        console.log("Projects button hovered!");
+
+        hoveredProject = true;
+        dropdown.style.display = "block";
+
     });
+
+    dropdown.addEventListener("mouseover", () => {
+
+        hoveredProject = true;
+        dropdown.style.display = "block";
+
+    });
+
+    projectsButton.addEventListener("mouseout", () => {
+
+        hoveredProject = false;
+        dropdown.style.display = "none";
+
+    });
+
+
 });
