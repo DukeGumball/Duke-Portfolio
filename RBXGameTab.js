b4d2fafba48c89d4bbf109939f66986b7a71f51c
DukeGumball <dukegumball@gmail.com>
2025-04-15 13:02:38 -0400

    ff

--- RBXGameTab.js
+++ RBXGameTab.js
@@ -37,23 +37,45 @@ document.addEventListener("DOMContentLoaded", async() => {
                 const titleTag = document.createElement("h2");
                 const aboutTag = document.createElement("p");
                 const mainImage = document.createElement("img");
+                const aboutLabel = document.createElement("h3");
+                const videoBox = document.createElement("div");
 
                 titleTag.id = "titleTag"
                 titleTag.innerHTML = showcase.name;
+                titleTag.style.textAlign = "center";
                 aboutTag.id = "aboutTag";
                 aboutTag.innerHTML = showcase.About;
                 mainImage.src = showcase.MainImage;
                 mainImage.id = "ProjectImage";
-
+                aboutLabel.id = "aboutLabel";
+                aboutLabel.innerHTML = "About:"
+                videoBox.id = "videoBox";
                 
                 showcaseLabel.appendChild(titleTag);
+                showcaseLabel.appendChild(aboutLabel);
                 showcaseLabel.appendChild(aboutTag);
                 showcaseLabel.appendChild(mainImage);
-
                 mainContent.appendChild(showcaseLabel);
+                showcaseLabel.appendChild(videoBox);
 
+                for (const video of showcase.videos) {
+                    if (video.includes("youtube.com")) {
+                        const videoId = video.split("v=")[1].split("&")[0]; 
+                        const iframe = document.createElement("iframe");
+                        iframe.src = `https://www.youtube.com/embed/${videoId}`;
+                        iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+                        iframe.allowFullscreen = true;
+                        iframe.id = "videoShowcase";
+                        videoBox.appendChild(iframe);
+                    } else if (video.endsWith(".mp4")) {
+                        const videoLabel = document.createElement("video");
+                        videoLabel.src = video;
+                        videoLabel.controls = true; 
+                        videoLabel.id = "videoShowcase";
+                        videoBox.appendChild(videoLabel);
+                    }
+                }
             }
-
         })
         .catch(error => {
             console.error('Error fetching projects:', error);
